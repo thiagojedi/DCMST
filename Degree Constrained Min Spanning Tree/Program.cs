ee822f023cfb3cc0dc4cd8b1869a978f193b19b3
Thiago Abreu <thiagoa7@gmail.com>
2013-11-19 13:13:30 -0300

    Cosmetic changes and performance theories

--- Degree Constrained Min Spanning Tree/Program.cs
+++ Degree Constrained Min Spanning Tree/Program.cs
@@ -126,28 +126,32 @@ namespace DCMSC_Exact
 
             foreach (var edge in nxy)
             {
-                if (!Helpers.CoveredEdge(x, edge.Item1, edge.Item2))
+                if (!Helpers.CoveredEdge(x, edge.Item1, edge.Item2) &&
+                    (!vertex_with_degrees.ContainsKey(edge.Item1) || vertex_with_degrees[edge.Item1] < this.degree) &&
+                    (!vertex_with_degrees.ContainsKey(edge.Item2) || vertex_with_degrees[edge.Item2] < this.degree))
+                {
                     x.Add(edge);
 
-                if (!vertex_with_degrees.ContainsKey(edge.Item1))
-                    vertex_with_degrees.Add(edge.Item1, 1);
-                else
-                    vertex_with_degrees[edge.Item1] += 1;
+                    if (!vertex_with_degrees.ContainsKey(edge.Item1))
+                        vertex_with_degrees.Add(edge.Item1, 1);
+                    else
+                        vertex_with_degrees[edge.Item1] += 1;
 
-                if (!vertex_with_degrees.ContainsKey(edge.Item2))
-                    vertex_with_degrees.Add(edge.Item2, 1);
-                else
-                    vertex_with_degrees[edge.Item2] += 1;
+                    if (!vertex_with_degrees.ContainsKey(edge.Item2))
+                        vertex_with_degrees.Add(edge.Item2, 1);
+                    else
+                        vertex_with_degrees[edge.Item2] += 1;
 
-                if (vertex_with_degrees[edge.Item1] >= this.degree)
-                {
-                    node_to_y = edge.Item1;
-                    break;
-                }
-                if (vertex_with_degrees[edge.Item2] >= this.degree)
-                {
-                    node_to_y = edge.Item2;
-                    break;
+                    if (vertex_with_degrees[edge.Item1] >= this.degree)
+                    {
+                        node_to_y = edge.Item1;
+                        break;
+                    }
+                    if (vertex_with_degrees[edge.Item2] >= this.degree)
+                    {
+                        node_to_y = edge.Item2;
+                        break;
+                    }
                 }
             }
             for (int i = 0; i < ordem; i++)
@@ -170,13 +174,13 @@ namespace DCMSC_Exact
             Random r = new Random();
 
             //Step 1
-            //start = r.Next(ordem);
+            start = r.Next(ordem);
             upper_bound = Helpers.Cost(this.PrimDegreeConstrained(start), this.initial_matrix);
             Console.WriteLine("Upper bound inicial: {0}", upper_bound);
             index = 0;
 
             //Step 2
-            //start = r.Next(ordem);
+            start = r.Next(ordem);
             List<Tuple<int, int>> not_in_xy = Prim(start);
             right_bound.Insert(index, Helpers.Cost(not_in_xy, this.initial_matrix));
             left_bound.Insert(index, int.MaxValue);
@@ -238,8 +242,10 @@ namespace DCMSC_Exact
                 {
                     List<Tuple<int, int>> outro_nxy = new List<Tuple<int, int>>(not_in_xy);
                     outro_nxy.Remove(ult_x);
-                    GenerateXY(ref x, ref old_y, outro_nxy);
-                    complete = CompleteMST(x, y);
+                    List<Tuple<int, int>> outro_old_y = new List<Tuple<int, int>>(old_y);
+                    outro_old_y.Add(ult_x);
+                    GenerateXY(ref x, ref outro_old_y, outro_nxy);
+                    complete = CompleteMST(x, outro_old_y);
                     left_bound[index] = Helpers.Cost(complete, this.initial_matrix);
                 }
                 else
@@ -256,7 +262,7 @@ namespace DCMSC_Exact
             }
 
             Console.WriteLine("Custo da Solução: {0}", Helpers.Cost(complete, this.initial_matrix));
-            Helpers.PrintEdges("Solução", complete, true);
+            //Helpers.PrintEdges("Solução", complete, true);
         }
 
         /// <summary>
@@ -322,7 +328,7 @@ namespace DCMSC_Exact
                 else
                 {
                     int i = 0;
-                    while (i < nl.Count && dic[edge] < dic[nl[i]])
+                    while (i < nl.Count && dic[edge] > dic[nl[i]])
                         i++;
                     nl.Insert(i, edge);
                 }
@@ -351,16 +357,16 @@ namespace DCMSC_Exact
             vs_in_t1.Add(edge.Item1);
             vs_in_t2.Add(edge.Item2);
 
-            while (t0.Count > 0)
+            while (vs_in_t1.Count + vs_in_t2.Count < this.ordem)
             {
-                var e = t0.First();
-                if (vs_in_t1.Contains(e.Item1) && !vs_in_t1.Contains(e.Item2))
+                var e = t0.Find(x => (vs_in_t1.Contains(x.Item1) || vs_in_t1.Contains(x.Item2) || vs_in_t2.Contains(x.Item1) || vs_in_t2.Contains(x.Item2)));
+                if (vs_in_t1.Contains(e.Item1))
                     vs_in_t1.Add(e.Item2);
-                else if (vs_in_t1.Contains(e.Item2) && !vs_in_t1.Contains(e.Item1))
+                else if (vs_in_t1.Contains(e.Item2))
                     vs_in_t1.Add(e.Item1);
-                else if (vs_in_t2.Contains(e.Item1) && !vs_in_t2.Contains(e.Item2))
+                else if (vs_in_t2.Contains(e.Item1))
                     vs_in_t2.Add(e.Item2);
-                else if (vs_in_t2.Contains(e.Item2) && !vs_in_t2.Contains(e.Item1))
+                else if (vs_in_t2.Contains(e.Item2))
                     vs_in_t2.Add(e.Item1);
 
                 t0.Remove(e);
@@ -380,7 +386,7 @@ namespace DCMSC_Exact
                 }
             }
 
-            int penality = Math.Abs((int)initial_matrix[edge.Item1][edge.Item2] - min);
+            int penality = (int)initial_matrix[edge.Item1][edge.Item2] - min;
             Debug.Print("Tuple {0}-{1}({2}) - Min {3}-{4}({5}) = {6}", edge.Item1 + 1, edge.Item2 + 1, (int)initial_matrix[edge.Item1][edge.Item2], min_i + 1, min_j + 1, min, penality);
             return penality;
         }
@@ -392,7 +398,7 @@ namespace DCMSC_Exact
             Console.WriteLine("Árvore Geradora Mínima de Grau Restrito");
             Console.WriteLine("---------------------------------------");
 
-            Console.WriteLine("Digite o nome do arquivo: ");
+            Console.WriteLine("Digite o nome do arquivo (tcXX-y | sendo XX = 40 ou 80, e y = [1..5]): ");
             string arquivo = Console.ReadLine();
             if (arquivo.Contains("40"))
                 Console.WriteLine("Digite o grau máximo [3, 5 ou 10]: ");
@@ -411,6 +417,8 @@ namespace DCMSC_Exact
             Console.WriteLine("Calculado em {0}s", (DateTime.Now - start).TotalSeconds);
 
             Console.WriteLine();
+
+            Console.WriteLine("Pressione ENTER para sair");
 
             Console.Read();
         }
--- Degree Constrained Min Spanning Tree/Properties/AssemblyInfo.cs
+++ Degree Constrained Min Spanning Tree/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Degree Constrained Min Spanning Tree")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("GPL v3.0")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
