9d7c1cb84a190df1b33e7ae27fa525dca45adec7
Thiago Abreu <thiagoa7@gmail.com>
2013-11-23 09:43:37 -0300

    AddedPher method and related parameters

--- Ant Colony/Program.cs
+++ Ant Colony/Program.cs
@@ -9,6 +9,51 @@ namespace Ants
 {
     class Program
     {
+        private int[,] WeightMatrix;
+        private double[,] PheromoneMatrix;
+
+        // Algorithm Parameters
+        private int MaxWeith;
+        private int MinWeith;
+
+        private double EvapFactor;
+
+
+        private double MaxPhero { get { return 1000 * ((MaxWeith - MinWeith) + (MaxWeith - MinWeith) / 3); } }
+        private double MinPhero { get { return (MaxWeith - MinWeith) / 3; } }
+
+
+
+        /// <summary>
+        /// Construtor padrão
+        /// </summary>
+        /// <param name="file">Nome do Arquivo de Testes</param>
+        /// <param name="graph_size">Número de Vértices no Grafo</param>
+        public Program(string file, int graph_size)
+        {
+            #region Inicialização de Variáveis
+            this.WeightMatrix = new int[graph_size, graph_size];
+            #endregion
+        }
+
+        /// <summary>
+        /// Calcula quanto deve ser adicionado de Feromônio por formiga
+        /// </summary>
+        /// <param name="edge">Aresta percorrida</param>
+        /// <returns>Incremento de Feromônio</returns>
+        double AddedPher(Tuple<int,int> edge)
+        {
+            int a = edge.Item1, b = edge.Item2;
+            if (a > b)
+            {
+                // XOR Swap
+                a = a ^ b;
+                b = a ^ b;
+                a = a ^ b;
+            }
+            return (MaxWeith - WeightMatrix[a, b]) + (MaxWeith - MinWeith) / 3;
+        }
+
         [STAThread]
         static void Main(string[] args)
         {
